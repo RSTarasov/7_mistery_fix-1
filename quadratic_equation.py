60c78d9e0f868e309b3105d404b13198b6a759a7
Denis Belevich <denis@belevich.net>
2017-05-09 20:06:12 +0700

    убрана лишняя вложенность

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,8 +3,7 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-
-    if discriminant < 0 or a != 0: return None, None
+    if discriminant < 0 or a == 0: return None, None
 
     root1 = (-b - sqrt(discriminant)) / (2 * a)
     root2 = (-b + sqrt(discriminant)) / (2 * a)
@@ -12,5 +11,3 @@ def get_roots(a, b, c):
         return root1, None
     else:
         return root1, root2
-
-print (get_roots(1,2,3))
